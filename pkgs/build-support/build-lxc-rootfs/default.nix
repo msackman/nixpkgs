c483860e8f4ffd522ea97dbdb5ac3f75eb655ca9
Matthew Sackman <matthew@rabbitmq.com>
2014-01-13 15:58:11 +0000

    And now we can write out a suitable fstab and set up the nix-store mounts in the rootfs

--- pkgs/build-support/build-lxc-rootfs/default.nix
+++ pkgs/build-support/build-lxc-rootfs/default.nix
@@ -23,7 +23,12 @@ in
     buildCommand = ''
       mkdir -p $out/rootfs
       mkdir -p $out/lxc
-      touch $out/pkgs
-      ${joinStrings "\n" (map (f: "cat ${f} >> $out/pkgs") depFiles)}
+      ${joinStrings "\n" (map (p: "printf \"%s\n\" \""+p+"\" >> pkgs") pkgs_)}
+      cat pkgs ${joinStrings " " depFiles} | sort | uniq | grep '^[^0-9]' > $out/dependencies
+
+      for dir in $(cat $out/dependencies); do
+        mkdir -p $out/rootfs$dir
+        printf "%s %s none ro,bind 0 0\n" $dir $out/rootfs$dir >> $out/lxc/fstab
+      done
     '';
   }
