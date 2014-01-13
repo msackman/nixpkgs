d2c736714b8998c3ed4c2d924615cd412fe246cf
Matthew Sackman <matthew@rabbitmq.com>
2014-01-13 14:58:54 +0000

    Avoid typeos, and Do It Better.

--- pkgs/build-support/build-lxc-rootfs/default.nix
+++ pkgs/build-support/build-lxc-rootfs/default.nix
@@ -5,9 +5,16 @@ assert pkgs == [] -> pkg != null;
 assert pkg == null -> pkgs != [];
 
 let
+  interleave = xs: ys:
+    if xs == []
+    then ys
+    else if ys == []
+      then xs
+      else [(builtins.head xs) (builtins.head ys)] ++
+           interleave (builtins.tail xs) (builtins.tail ys);
   pkgs_ = if pkgs == [] then [pkg] else pkgs;
   depFiles = map baseNameOf pkgs_;
-  pkgsDeps = lib.zipTwoLists depFile pkgs_;
+  pkgsDeps = interleave depFiles pkgs_;
   joinStrings = sep: lib.fold (e: acc: e + sep + acc) "";
 in
   stdenv.mkDerivation {
@@ -17,8 +24,6 @@ in
       mkdir -p $out/rootfs
       mkdir -p $out/lxc
       touch -p $out/pkgs
-      for p in ${joinStrings " " depFiles}; do
-        cat $p >> $out/pkgs
-      done
+      ${map (f: "cat ${f} >> $out\n") depFiles}
     '';
   }
