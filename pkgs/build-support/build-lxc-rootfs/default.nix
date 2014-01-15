643f8258af84f0ee32f8a5098f17a57a08cf5122
Matthew Sackman <matthew@rabbitmq.com>
2014-01-15 11:53:03 +0000

    Rework lxc config defaults so it's a list of attrsets. This is necessary because order of terms in the lxc config file is important. Then for better control, actually pass it via an optional function provided in the args to allow customisation. It does all seem to work.

--- pkgs/build-support/build-lxc-rootfs/default.nix
+++ pkgs/build-support/build-lxc-rootfs/default.nix
@@ -1,5 +1,5 @@
 {stdenv, lib}:
-{name, pkgs ? [], pkg ? null, lxc ? {}}:
+{name, pkgs ? [], pkg ? null, lxcFun ? lib.id}:
 
 assert pkgs == [] -> pkg != null;
 assert pkg == null -> pkgs != [];
@@ -13,60 +13,52 @@ let
       else [(builtins.head xs) (builtins.head ys)] ++
            interleave (builtins.tail xs) (builtins.tail ys);
   joinStrings = sep: lib.fold (e: acc: e + sep + acc);
-  foldAttrSet = op: init: attrs:
-    lib.fold (name: acc: op name (builtins.getAttr name attrs) acc)
-             init (builtins.attrNames attrs);
-  attrSetToString = rec { f = (path: init:
-    foldAttrSet (name: value: acc:
-        if builtins.isAttrs value then
-          f (path + name + ".") acc value
-        else if builtins.isList value then
-          joinStrings "\n" acc (map (e: if builtins.isAttrs e then
-             f (path+name+".") "" e
-           else
-             path + name + " = " + (toString e)) value)
-        else
-          path + name + " = " + (toString value) + "\n" + acc) init);
-    }.f;
 
   pkgs_ = if pkgs == [] then [pkg] else pkgs;
   depFiles = map baseNameOf pkgs_;
   pkgsDeps = interleave depFiles pkgs_;
 
-  lxcConfigDefaults = {
-    network.type = "empty";                # network is disabled by default
-    pts = 1024;                            # use a dedicated pts for the container
-    console = "none";                      # disable main console
-    tty = 1;                               # no controlling tty at all
+  lxcConfigDefaults = [
+    {"network.type" = "empty";}           # network is disabled by default
+    {pts = 1024;}                         # use a dedicated pts for the container
+    {console = "none";}                   # disable main console
+    {tty = 1;}                            # no controlling tty at all
 
-    cgroup.devices.deny = "a";             # no implicit access to devices
-    cgroup.devices.allow =
-      ["c 1:3 rwm"   "c 1:5 rwm" # /dev/null and zero
-       "c 5:1 rwm"   "c 5:0 rwm" # consoles
-       "c 4:0 rwm"   "c 4:1 rwm"
-       "c 1:9 rwm"   "c 1:8 rwm" # /dev/urandom,/dev/random
-       "c 136:* rwm" "c 5:2 rwm" # /dev/pts/ - pts namespaces are "coming soon"
-       "c 10:200 rwm"            # tuntap
-      ];
+    {"cgroup.devices.deny" = "a";}          # no implicit access to devices
+    {"cgroup.devices.allow" = "c 1:3 rwm"   ;} # /dev/null and zero
+    {"cgroup.devices.allow" = "c 1:5 rwm"   ;}
+    {"cgroup.devices.allow" = "c 5:1 rwm"   ;} # consoles
+    {"cgroup.devices.allow" = "c 5:0 rwm"   ;}
+    {"cgroup.devices.allow" = "c 4:0 rwm"   ;}
+    {"cgroup.devices.allow" = "c 4:1 rwm"   ;}
+    {"cgroup.devices.allow" = "c 1:9 rwm"   ;} # /dev/urandom,/dev/random
+    {"cgroup.devices.allow" = "c 1:8 rwm"   ;}
+    {"cgroup.devices.allow" = "c 136:* rwm" ;} # /dev/pts/ - pts namespaces are "coming soon"
+    {"cgroup.devices.allow" = "c 5:2 rwm"   ;}
+    {"cgroup.devices.allow" = "c 10:200 rwm";} # tuntap
 
-    pivotdir = "lxc_putold";
-    mount = ["@lxcDir@/lib/fstab"
-             {entry = [
+    {pivotdir = "lxc_putold";}
+    {mount = "@lxcDir@/lib/fstab";}
     #  WARNING: procfs is a known attack vector and should probably be disabled
     #           if your userspace allows it. eg. see http://blog.zx2c4.com/749
-             "proc @lxcDir@/rootfs/proc proc nosuid,nodev,noexec 0 0"
-
+    {"mount.entry" =
+       "proc @lxcDir@/rootfs/proc proc nosuid,nodev,noexec 0 0";}
     # WARNING: sysfs is a known attack vector and should probably be disabled
     # if your userspace allows it. eg. see http://bit.ly/T9CkqJ
-             "sysfs @lxcDir@/rootfs/sys sysfs nosuid,nodev,noexec 0 0"];}];
+    {"mount.entry" =
+       "sysfs @lxcDir@/rootfs/sys sysfs nosuid,nodev,noexec 0 0";}
 
-    autodev = 1;
-    rootfs = "@lxcDir@/rootfs";
-  };
+    {autodev = 1;}
+    {rootfs = "@lxcDir@/rootfs";}
+  ];
 
-  lxcConfig = lib.recursiveUpdate lxcConfigDefaults lxc;
+  lxcConfig = lxcFun lxcConfigDefaults;
   lxcConfigFile = builtins.toFile "lxc.config.in"
-    (attrSetToString "lxc." "" lxcConfig);
+    (joinStrings "\n" "" (
+      map (attrs: lib.fold (
+        name: acc: "lxc."+name+ " = "+(toString (builtins.getAttr name attrs))
+        ) "" (builtins.attrNames attrs)) lxcConfig
+    ));
   mountPoints = ["proc" "sys" "dev"]; #manually keep in sync with lxc.mount.entry
 
   createSh = ./create-rootfs.sh.in;
