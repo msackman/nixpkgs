2fb60946c6885dc8e7c32057918217173a1dcd46
Matthew Sackman <matthew@rabbitmq.com>
2014-01-27 11:56:43 +0000

    Bump rabbit to 3.2.2 and bring down the clusterer too. Not yet worked out how to combine building the clusterer into the rest, but can't be too hard...

---
+++ pkgs/servers/amqp/tsp-rabbitmq-server/default.nix
@@ -0,0 +1,51 @@
+{ stdenv, fetchurl, erlang, python, libxml2, libxslt, xmlto
+, docbook_xml_dtd_45, docbook_xsl, zip, unzip }:
+
+stdenv.mkDerivation rec {
+  name = "tsp-rabbitmq-server-${version}";
+
+  version = "3.2.2";
+
+  rabbit = fetchurl {
+    url = "http://www.rabbitmq.com/releases/rabbitmq-server/v${version}/${name}.tar.gz";
+    sha256 = "c6f985d2bf69de60fa543ebfff190f233d2ab8faee78a10cfb065b4e4d1406ba";
+  };
+
+  clusterer = fetchhg {
+    url = "http://rabbit-hg-private/rabbitmq-clusterer";
+    sha256 = "0kjzbirzbay7svk19r94b1qspgglw21by922bivil84d6i5qimk2";
+  }
+
+  srcs = [ rabbit, clusterer ];
+  sourceRoot = rabbit;
+
+  buildInputs =
+    [ erlang python libxml2 libxslt xmlto docbook_xml_dtd_45 docbook_xsl zip unzip ];
+
+  preBuild =
+    ''
+      # Fix the "/usr/bin/env" in "calculate-relative".
+      patchShebangs .
+    '';
+
+  installFlags = "TARGET_DIR=$(out)/libexec/rabbitmq SBIN_DIR=$(out)/sbin MAN_DIR=$(out)/share/man DOC_INSTALL_DIR=$(out)/share/doc";
+
+  preInstall =
+    ''
+      sed -i \
+        -e 's|SYS_PREFIX=|SYS_PREFIX=''${SYS_PREFIX-''${HOME}/.rabbitmq/${version}}|' \
+        -e 's|CONF_ENV_FILE=''${SYS_PREFIX}\(.*\)|CONF_ENV_FILE=\1|' \
+        scripts/rabbitmq-defaults
+    '';
+
+  postInstall =
+    ''
+      echo 'PATH=${erlang}/bin:${PATH:+:}$PATH' >> $out/sbin/rabbitmq-env
+    ''; # */
+
+  meta = {
+    homepage = http://www.rabbitmq.com/;
+    description = "An implementation of the AMQP messaging protocol";
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
