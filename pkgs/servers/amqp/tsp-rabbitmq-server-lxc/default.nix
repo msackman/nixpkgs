efc1088688757b6157fe9103344c76900d2d10d4
Matthew Sackman <matthew@rabbitmq.com>
2014-01-29 18:07:22 +0000

    Updating the example.

--- pkgs/servers/amqp/tsp-rabbitmq-server-lxc/default.nix
+++ pkgs/servers/amqp/tsp-rabbitmq-server-lxc/default.nix
@@ -1,18 +1,18 @@
-{ tsp_rabbitmq_server, buildLXCRootFS, lib }:
+{ tsp_rabbitmq_server, buildLXC, lib }:
 
-buildLXCRootFS {
+buildLXC {
   name = "rabbitmq-server-lxc";
   pkgs = [ tsp_rabbitmq_server ];
-  lxcFun = libraryPath: ''
-    let lxcConfigLib = (import ${libraryPath}) lib; in
-    {configFun = lxcConfigLib.addNetwork {
-      type = "veth";
-      link = "br0";
-      name = "eth0";
-      flags = "up";
-      ipv4 = "192.168.99.99";
+  lxcConf = ''lxcConfLib:
+    {conf = lxcConfLib.addNetwork {
+      type           = "veth";
+      link           = "br0";
+      name           = "eth0";
+      flags          = "up";
+      ipv4           = "192.168.99.99";
       "ipv4.gateway" = "192.168.99.1";};
-     execFun = "${tsp_rabbitmq_server}/sbin/rabbitmq-server";
+     exec = "${tsp_rabbitmq_server}/sbin/rabbitmq-server";
+     lxcPkgs = [ ];
     }
     '';
 }
