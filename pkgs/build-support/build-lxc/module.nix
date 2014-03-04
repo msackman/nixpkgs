213b946bf6c1975daa653a83f0d5cce0424a3341
Matthew Sackman <matthew@rabbitmq.com>
2014-03-04 15:41:04 +0000

    Well that was surprisingly simple. We can now start any lxc from systemd.
    For example, in /etc/nixos/configuration.nix, I now have:
      imports =
        [ # Include the results of the hardware scan.
          ./hardware-configuration.nix
    
          ((import /root/nixpkgs/pkgs/build-support/build-lxc/module.nix) (import /root/serfdom.nix))
          ((import /root/nixpkgs/pkgs/build-support/build-lxc/module.nix) (import /root/router.nix))
          ((import /root/nixpkgs/pkgs/build-support/build-lxc/module.nix) (import /root/erlinetrc.nix))
        ];
    
    and then a "nixos-rebuild -I nixpkgs=/root/nixpkgs/pkgs switch" makes it all work.
    I might change the API so that we can pass pkgs through (i.e. buildLXC provides it). That way we can drop the -I in the above. We will also want the service configs to be able to express dependencies between each other and have that exposed to systemd, but that doesn't seem a huge challenge. But this does appear to Just Work.

---
+++ pkgs/build-support/build-lxc/module.nix
@@ -0,0 +1,66 @@
+lxcDesc:
+  { config, pkgs, ... }:
+    with pkgs.lib;
+    let
+      name = lxcDesc.name;
+      cfg = builtins.getAttr name config.services.lxc;
+      createScript = lxcDesc.scripts + "/bin/lxc-create-${name}";
+      startScript = lxcDesc.scripts + "/bin/lxc-start-${name}";
+    in
+    {
+      ###### interface
+      options = {
+        services.lxc = builtins.listToAttrs [
+        { inherit name;
+          value = {
+            enable = mkOption {
+              type = types.bool;
+              default = false;
+              description = ''
+                Whether to run the ${name} container.
+              '';
+            };
+            containerPath = mkOption {
+              type = types.path;
+              default = "/var/lxc/${name}";
+              description = ''
+                Path to the container.
+              '';
+            };
+            containerName = mkOption {
+              type = types.str;
+              default = "${name}";
+              description = ''
+                The name to run the contain as.
+              '';
+            };
+          };
+        }];
+      };
+
+      ###### implementation
+      config = mkIf cfg.enable {
+        environment.systemPackages = [pkgs.lxc pkgs.bash];
+        systemd.services = builtins.listToAttrs [{
+          inherit name;
+          value = {
+            description = "LXC container: ${name}";
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
+            preStart = ''
+              if [ ! -d "${cfg.containerPath}" ]; then
+                mkdir -p $(dirname "${cfg.containerPath}")
+                ${createScript} ${cfg.containerPath}
+              fi
+            '';
+            serviceConfig = {
+              ExecStart = "${startScript} ${cfg.containerPath} ${cfg.containerName}";
+              ExecStop = "${pkgs.lxc}/bin/lxc-stop -k -n ${cfg.containerName}";
+              Type = "forking";
+              Restart = "always";
+            };
+            unitConfig.RequiresMountsFor = "${cfg.containerPath}";
+          };
+        }];
+      };
+    }
