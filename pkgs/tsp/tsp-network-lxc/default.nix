b07c46b9677cc4901938c7c291be871d7f4fe9c1
Matthew Sackman <matthew@rabbitmq.com>
2014-03-20 17:36:45 +0000

    Do said network rewrite. Careful use of foldl ensures that the lists all stay in the same order, so eth0 should be at the correct end of the list (i.e. front).
    Ultimately we may well want to use a global network grab... Or enforce singleton somehow.

--- pkgs/tsp/tsp-network-lxc/default.nix
+++ pkgs/tsp/tsp-network-lxc/default.nix
@@ -2,77 +2,87 @@
 
 tsp.container ({ global, configuration, containerLib }:
   let
-    inherit (lib) fold splitString optionalString;
+    inherit (lib) fold foldl splitString optionalString;
     inherit (builtins) getAttr attrNames hasAttr listToAttrs filter length isString elem elemAt head;
     tsp_systemd_units = callPackage ../tsp-systemd-units-lxc { };
     name = "network";
+
     baseNetwork = {
-      type  = "veth";
+      type  = "veth"; ## In truth, we don't support anything else just ATM.
       link  = "br0";
       flags = "up";
     };
     validKeys = (attrNames baseNetwork) ++ ["ipv4" "ipv4.gateway" "mtu" "hwaddr"];
-    networkConfiguration = network: fold (name: acc:
-      acc ++ (if hasAttr name network then
-                [{inherit name; value = getAttr name network;}]
-              else
-                []
-             )) [] validKeys;
-    nic = network: baseNetwork // (listToAttrs (networkConfiguration network));
 
-    addNetwork = network:
-      let fullNetwork = nic network; in
-      { name = "interface"; type = "bridge";
-        value = [
-            { name = "source"; bridge = fullNetwork.link; }
-          ] ++ (if fullNetwork ? hwaddr then
-                  [{ name = "mac"; address = fullNetwork.hwaddr; }]
-                else
-                  []);
-      };
-    networks = map addNetwork configuration.networks;
+    extendNetwork = network:
+      baseNetwork // (listToAttrs (fold (key: acc:
+        if hasAttr key network then
+          [{ name = key; value = getAttr key network; }] ++ acc
+        else
+          acc
+      ) [] validKeys));
 
-    systemdGuestService = nicNum: network:
+    configureNetwork = nicNum: network:
       let
-        fullNetwork = nic network;
+        guestNicName = "eth${toString nicNum}";
+        fullNetwork = extendNetwork network;
         ipPrefix = lib.splitString "/" fullNetwork.ipv4;
         ipv4 = head ipPrefix;
         mask = if length ipPrefix == 2 then elemAt ipPrefix 1 else "24";
-        nicName = "eth${toString nicNum}"; # NB this is *highly* libvirt specific
       in
         {
-          name = "network-${nicName}";
-          value =
+          containerConf =
+            { name = "interface"; type = "bridge";
+              value = [
+                  { name = "source"; bridge = fullNetwork.link; }
+                ] ++ (if fullNetwork ? hwaddr then
+                        [{ name = "mac"; address = fullNetwork.hwaddr; }]
+                      else
+                        []);
+            };
+          systemd_unit_pair =
             {
-              description = "Network interface configuration for ${nicName}";
-              wantedBy = [ "network.target" ];
-              requires = [];
-              after = [];
-              serviceConfig.Type = "oneshot";
-              serviceConfig.RemainAfterExit = true;
-              script =
-                ''
-                  printf "bringing up interface...\n"
-                  ${iproute}/sbin/ip link set "${nicName}" up
-                ''
-                + optionalString (fullNetwork ? hwaddr)
-                  ''
-                    printf "setting MAC address to ${fullNetwork.hwaddr}...\n"
-                    ${iproute}/sbin/ip link set "${nicName}" address "${fullNetwork.hwaddr}"
-                  ''
-                + optionalString (fullNetwork ? mtu)
-                  ''
-                    printf "setting MTU to ${toString fullNetwork.mtu}...\n"
-                    ${iproute}/sbin/ip link set "${nicName}" mtu "${toString fullNetwork.mtu}"
-                  ''
-                + optionalString (fullNetwork ? ipv4)
-                  ''
-                    printf "configuring interface...\n"
-                    ${iproute}/sbin/ip -4 addr flush dev "${nicName}"
-                    ${iproute}/sbin/ip -4 addr add "${ipv4}/${mask}" dev "${nicName}"
-                  '';
+              name = "network-${guestNicName}";
+              value =
+                {
+                  description = "Network interface configuration for ${guestNicName}";
+                  wantedBy = [ "network.target" ];
+                  requires = [];
+                  after = [];
+                  serviceConfig.Type = "oneshot";
+                  serviceConfig.RemainAfterExit = true;
+                  script =
+                    ''
+                      printf "bringing up interface...\n"
+                      ${iproute}/sbin/ip link set "${guestNicName}" up
+                    ''
+                    + optionalString (fullNetwork ? hwaddr)
+                      ''
+                        printf "setting MAC address to ${fullNetwork.hwaddr}...\n"
+                        ${iproute}/sbin/ip link set "${guestNicName}" address "${fullNetwork.hwaddr}"
+                      ''
+                    + optionalString (fullNetwork ? mtu)
+                      ''
+                        printf "setting MTU to ${toString fullNetwork.mtu}...\n"
+                        ${iproute}/sbin/ip link set "${guestNicName}" mtu "${toString fullNetwork.mtu}"
+                      ''
+                    + optionalString (fullNetwork ? ipv4)
+                      ''
+                        printf "configuring interface...\n"
+                        ${iproute}/sbin/ip -4 addr flush dev "${guestNicName}"
+                        ${iproute}/sbin/ip -4 addr add "${ipv4}/${mask}" dev "${guestNicName}"
+                      '';
+                };
             };
         };
+
+    networks =
+      let nets = configuration.networks; in
+      (foldl ({num, list}: network:
+         {
+           num  = num + 1;
+           list = list ++ [(configureNetwork num network)];
+         }) {num = 0; list = [];} nets).list;
 
     createIn = ./on-create.sh.in;
     steriliseIn = ./on-sterilise.sh.in;
@@ -97,17 +107,11 @@ tsp.container ({ global, configuration, containerLib }:
   in
   {
     name = "${name}-lxc";
-    containerConf = containerLib.extendContainerConf ["devices"] networks;
+    containerConf = containerLib.extendContainerConf ["devices"] (map (e: e.containerConf) networks);
     onCreate = [ create ];
     onSterilise = [ sterilise ];
     storeMounts = { systemd_units = tsp_systemd_units; };
-    configuration = { systemd_units.systemd_services =
-                        listToAttrs (
-                          fold (network: {num, list}: {
-                            num = num + 1;
-                            list = list ++ [(systemdGuestService num network)];
-                          }) { num = 0; list = []; } configuration.networks).list;
-                    };
+    configuration = { systemd_units.systemd_services = listToAttrs (map (e: e.systemd_unit_pair) networks); };
     options = {
       hostname = containerLib.mkOption { optional = true; };
       networks = containerLib.mkOption {
@@ -118,12 +122,9 @@ tsp.container ({ global, configuration, containerLib }:
                        fold (name: acc:
                          let value = getAttr name network; in
                          assert elem name validKeys;
-                         if (name == "ipv4") || (name == "ipv4.gateway" && value != "auto") then
+                         if (name == "ipv4") || (name == "ipv4.gateway") then
                            let components = splitString "." value; in
                            assert length components == 4;
-                           acc
-                         else if name == "ipv4.gateway" then
-                           assert value == "auto";
                            acc
                          else if name == "hwaddr" then
                            let components = splitString ":" value; in
